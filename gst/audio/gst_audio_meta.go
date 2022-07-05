6561655cab50cc504de066276a0680f08c4efef1
Dan Jenkins <dan@nimblea.pe>
2022-07-05 23:07:40 +0100

    pass in NULL/nil temporarily

--- gst/audio/gst_audio_meta.go
+++ gst/audio/gst_audio_meta.go
@@ -20,12 +20,17 @@ func wrapMetaFull(ptr *C.GstAudioMeta) *AudioMeta {
 	return meta
 }
 
-func BufferAddAudioMeta(buffer *gst.Buffer, info *Info, samples int64, offsets []int) *AudioMeta {
-	gSizeOffsets := C.gsize(unsafe.Sizeof(unsafe.Pointer(&offsets)))
+func BufferAddAudioMeta(buffer *gst.Buffer, info *Info, samples int /*, offsets *[]int*/) *AudioMeta {
+	/*gSizeOffsets := C.gsize(unsafe.Sizeof(unsafe.Pointer(offsets)))*/
+	// if you pass in NULL as the last param then gstreamer assumes things are tightly packed...
+	// and they are for me so this makes things work... but obviously would be nice for it to work
+	// properly
+
 	return wrapMetaFull(C.gst_buffer_add_audio_meta(
 		(*C.GstBuffer)(unsafe.Pointer(buffer.Instance())),
 		info.ptr,
 		C.gsize(samples),
-		&gSizeOffsets,
+		/*&gSizeOffsets,*/
+		nil,
 	))
 }
