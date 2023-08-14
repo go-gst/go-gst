71805981619f23c9f5a8eea3a501cd2f99515400
RSWilli <bartel.wilhelm@gmail.com>
2023-08-14 11:38:34 +0200

    interpolationcontrol source set timed value

--- gst/control_source.go
+++ gst/control_source.go
@@ -3,12 +3,26 @@ package gst
 // #include "gst.go.h"
 import "C"
 import (
+	"strconv"
 	"unsafe"
 
 	"github.com/go-gst/go-glib/glib"
 )
 
 type InterpolationControlSource struct{ *Object }
+
+type InterpolationMode int
+
+const (
+	//steps-like interpolation, default
+	InterpolationModeNone InterpolationMode = 0 // GST_INTERPOLATION_MODE_NONE
+	//linear interpolation
+	InterpolationModeLinear InterpolationMode = 1 // GST_INTERPOLATION_MODE_LINEAR
+	//cubic interpolation (natural), may overshoot the min or max values set by the control point, but is more 'curvy'
+	InterpolationModeCubic InterpolationMode = 2 // GST_INTERPOLATION_MODE_CUBIC
+	//monotonic cubic interpolation, will not produce any values outside of the min-max range set by the control points (Since: 1.8)
+	InterpolationModeCubicMonotonic InterpolationMode = 3 // GST_INTERPOLATION_MODE_CUBIC_MONOTONIC
+)
 
 func (cs *InterpolationControlSource) Instance() *C.GstControlSource {
 	return C.toGstControlSource(cs.Unsafe())
@@ -22,6 +36,10 @@ func NewInterpolationControlSource() *InterpolationControlSource {
 	}
 }
 
-func (cs *InterpolationControlSource) SetTimedValue(time ClockTime, value float64) {
-	C.gst_timed_value_control_source_set(C.toGstTimedValueControlSource(cs.Unsafe()), C.ulong(time), C.double(value))
+func (cs *InterpolationControlSource) SetInterpolationMode(mode InterpolationMode) {
+	cs.SetArg("mode", strconv.Itoa(int(mode)))
+}
+
+func (cs *InterpolationControlSource) SetTimedValue(time ClockTime, value float64) bool {
+	return gobool(C.gst_timed_value_control_source_set(C.toGstTimedValueControlSource(cs.Unsafe()), C.ulong(time), C.double(value)))
 }
