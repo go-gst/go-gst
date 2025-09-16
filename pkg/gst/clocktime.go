6b4a23c6174974ad51f8ffb3fcdb30c9aa17936f
RSWilli <bartel.wilhelm@gmail.com>
2025-09-16 22:36:09 +0200

    add convenience clocktime constants

--- pkg/gst/clocktime.go
+++ pkg/gst/clocktime.go
@@ -5,6 +5,16 @@ import "time"
 // ClockTimeNone is a constant that represents an infinite or non-existent time.
 const ClockTimeNone ClockTime = 0xFFFFFFFFFFFFFFFF
 
+// Common durations type casted from time.Duration to ClockTime.
+const (
+	NanoSecond  ClockTime = ClockTime(time.Nanosecond)
+	MicroSecond           = ClockTime(time.Microsecond)
+	Millisecond           = ClockTime(time.Millisecond)
+	Second                = ClockTime(time.Second)
+	Minute                = ClockTime(time.Minute)
+	Hour                  = ClockTime(time.Hour)
+)
+
 // ClockTime wraps GstClockTime
 //
 // A datatype to hold a time, measured in nanoseconds.
