f799d8aca6ca47537083cc593d9ee7f22f855dc7
RSWilli <bartel.wilhelm@gmail.com>
2025-09-16 22:36:06 +0200

    generate bindings with gotk4 and deprecate old bindings
    adapt structure from gotk4
    deprecate old gst package

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Golang+gstreamer dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        go
+        pkg-config
+  
+        gst_all_1.gstreamer
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-editing-services
+        gst_all_1.gst-plugins-rs
+        gst_all_1.gst-libav
+        gst_all_1.gst-vaapi
+        gst_all_1.gst-rtsp-server
+
+        #gotk4 deps:
+        atk
+        gtk3
+        gtk4
+        glib
+        graphene
+        gdk-pixbuf
+        gobject-introspection
+        librsvg
+      ];
+
+      GO111MODULE="on";
+
+      # needed for running delve
+      # https://github.com/go-delve/delve/issues/3085
+      # https://nixos.wiki/wiki/C#Hardening_flags
+      hardeningDisable = [ "all" ];
+
+      # print the go version and gstreamer version on shell startup
+      shellHook = ''
+        ${pkgs.go}/bin/go version
+        ${pkgs.gst_all_1.gstreamer}/bin/gst-launch-1.0 --version
+      '';
+    };
+  };
+}
