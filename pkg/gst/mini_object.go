3d5a537374c9eed680514cd9b18ed21bc3f22829
RSWilli <bartel.wilhelm@gmail.com>
2025-12-04 16:51:19 +0100

    fix sync bus message unrefs

--- pkg/gst/mini_object.go
+++ pkg/gst/mini_object.go
@@ -11,12 +11,16 @@ import "C"
 
 // miniObjectRef calls gst_mini_object_ref on the given pointer. It is used for MiniObject extending
 // records, because the generator does not have the hierarchy to be able to figure out the casting.
+//
+// ptr must be a valid C pointer to a GstMiniObject or extending record.
 func miniObjectRef(ptr unsafe.Pointer) {
 	C.gst_mini_object_ref((*C.GstMiniObject)(ptr))
 }
 
 // miniObjectUnref calls gst_mini_object_unref on the given pointer. It is used for MiniObject extending
 // records, because the generator does not have the hierarchy to be able to figure out the casting.
+//
+// ptr must be a valid C pointer to a GstMiniObject or extending record.
 func miniObjectUnref(ptr unsafe.Pointer) {
 	C.gst_mini_object_unref((*C.GstMiniObject)(ptr))
 }
